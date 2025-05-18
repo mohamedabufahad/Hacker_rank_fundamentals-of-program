9b7577bf4a0f0282b030c792ebfd5e1d52c4812d
mohamedabufahad <abufahad7705@gmail.com>
2025-05-18 18:20:46 +0530

    ASCII Values - II.java

---
+++ ASCII Values - II.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int asciiValue = sc.nextInt();
+        char character = (char) asciiValue;
+        System.out.println(character);
+    }
+}
